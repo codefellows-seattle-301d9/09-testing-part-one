012d4326c196da58604af158c414f394881334bd
dougachrist <dougachrist@gmail.com>
2016-07-21 16:30:10 -0700

    wrtie testing cases

--- starter-code/bonus-testing-part-one.js
+++ starter-code/bonus-testing-part-one.js
@@ -5,7 +5,6 @@
     state of a particular place in our application.
 */
 
-
 /* Below is a basic example of a test "framework" where
     our test is a function that we can reuse to check against
     different components of our application.
@@ -25,12 +24,12 @@ function expect(expression, failureMessage, successMessage) {
     to see what happens when the test fails, then change `ricksFaveAnimal`
     to get the test to pass!
 */
-var ricksFaveAnimal = 'hyena';
-
-expect(
-  ricksFaveAnimal === 'penguin',
-  'ricksFavoriteAnimal should equal penguin, but currently equals ' + ricksFaveAnimal,
-  'ricksFavoriteAnimal equals penguin!');
+// var ricksFaveAnimal = 'hyena';
+//
+// expect(
+//   ricksFaveAnimal === 'penguin',
+//   'ricksFavoriteAnimal should equal penguin, but currently equals ' + ricksFaveAnimal,
+//   'ricksFavoriteAnimal equals penguin!');
 
   // BEGIN WORK BELOW - test code by running `node bonus-testing-part-one.js`
   //  in your terminal!
@@ -46,17 +45,23 @@ var favoriteAnimals = ['elephants', 'penguins', 'eagles', 'camels'];
 var nextAnimal;
 
   /* TODO:
-      Assign one of your favorite animals dynamically by chance to the 
+      Assign one of your favorite animals dynamically by chance to the
       nextAnimal variable   :-)
       Your code begins on the next line: */
+function getRandomIntInclusive(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+nextAnimal = favoriteAnimals[getRandomIntInclusive(favoriteAnimals.length - 1)];
 
   /* TODO:
       Write a test! Use the `expect()` function we defined earlier to ensure
       that an element in the favoriteAnimals array was assigned to nextAnimal.
-      No hard-coded results allowed! (ex: seeing if nextAnimal is equal to 
+      No hard-coded results allowed! (ex: seeing if nextAnimal is equal to
       just 'penguin').
       Remember to: pass in your expression, and write a failure and a success
       message. Your test begins on the next line: */
-
-
-
+expect(
+  (favoriteAnimals.indexOf(nextAnimal) + 1),
+   'You went to see the ' + nextAnimal + ' but they are not one of your favorite animals...',
+   'YIPPEE! You got to see the ' + nextAnimal + ' which are one of your favorite animals');
